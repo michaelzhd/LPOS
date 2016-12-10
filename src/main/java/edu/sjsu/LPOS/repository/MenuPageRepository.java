92742894c014a241e9fdef420050f66e80941565
Michael Dong <michaeldongsoft@gmail.com>
2016-12-09 17:31:21 -0800

    added elasticsearch/logstash/kibana support

--- src/main/java/edu/sjsu/LPOS/repository/MenuPageRepository.java
+++ src/main/java/edu/sjsu/LPOS/repository/MenuPageRepository.java
@@ -1,12 +1,12 @@
 package edu.sjsu.LPOS.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import edu.sjsu.LPOS.domain.Menu;
 
 public interface MenuPageRepository extends PagingAndSortingRepository<Menu,Integer> {
-	List<Menu> findByRestaurant_id(Integer id);
+	Page<Menu> findByRestaurant_id(Integer id, Pageable pageable);
 
 }
